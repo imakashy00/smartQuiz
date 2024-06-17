8e3e56021f81d05f42a382f94bd5d97917de3ceb
AKASH YADAV <yakashadav26@gmail.com>
2024-06-17 11:49:32 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse  # Add this import
 import json
 import uvicorn
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,20 +14,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
 @app.get('/')
 async def quiz():
     with open('questions.json') as q:
         data = json.load(q)
         return data
 
-
 @app.head("/")
 async def head_root():
     return JSONResponse(status_code=200)
-
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")

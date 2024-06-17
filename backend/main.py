86882d9630fd200030669e54352cc87e46f7aa8a
AKASH YADAV <yakashadav26@gmail.com>
2024-06-17 11:43:51 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,11 @@ async def quiz():
         return data
 
 
+@app.head("/")
+async def head_root():
+    return JSONResponse(status_code=200)
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
